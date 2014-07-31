008f88eaf0064a7814117f99139d36c203acdf92
avnerkam <avnerkam@users.noreply.github.com>
2014-07-31 15:05:11 -0400

    Update twitterstream.py

--- assignment1/twitterstream.py
+++ assignment1/twitterstream.py
@@ -3,59 +3,9 @@ import urllib2 as urllib
 
 # See assignment1.html instructions or README for how to get these credentials
 
-api_key = "cc87reVsbNU8LwRnPVjj9h1v3"
-api_secret = "CUOYdQPa7UXrwoisxsmrkrglMHM7wgKito0YV4DM30RMKeglR0"
-access_token_key = "37664732-kXmviN1HVPzldnlmTAjjhswkajQoMKisTiJOsauS1"
-access_token_secret = "6A3ftE0QGfoIMFlA11hHiQBlw08LBJPci19yYFl7LLgJH"
-
-_debug = 0
-
-oauth_token    = oauth.Token(key=access_token_key, secret=access_token_secret)
-oauth_consumer = oauth.Consumer(key=api_key, secret=api_secret)
-
-signature_method_hmac_sha1 = oauth.SignatureMethod_HMAC_SHA1()
-
-http_method = "GET"
 
 
-http_handler  = urllib.HTTPHandler(debuglevel=_debug)
-https_handler = urllib.HTTPSHandler(debuglevel=_debug)
-
-'''
-Construct, sign, and open a twitter request
-using the hard-coded credentials above.
-'''
-def twitterreq(url, method, parameters):
-  req = oauth.Request.from_consumer_and_token(oauth_consumer,
-                                             token=oauth_token,
-                                             http_method=http_method,
-                                             http_url=url, 
-                                             parameters=parameters)
-
-  req.sign_request(signature_method_hmac_sha1, oauth_consumer, oauth_token)
-
-  headers = req.to_header()
-
-  if http_method == "POST":
-    encoded_post_data = req.to_postdata()
-  else:
-    encoded_post_data = None
-    url = req.to_url()
-
-  opener = urllib.OpenerDirector()
-  opener.add_handler(http_handler)
-  opener.add_handler(https_handler)
-
-  response = opener.open(url, encoded_post_data)
-
-  return response
-
-def fetchsamples():
-  url = "https://stream.twitter.com/1/statuses/sample.json"
-  parameters = []
-  response = twitterreq(url, "GET", parameters)
-  for line in response:
-    print line.strip()
+ 
 
 if __name__ == '__main__':
   fetchsamples()
